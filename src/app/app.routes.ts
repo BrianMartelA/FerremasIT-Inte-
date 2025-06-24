03c55a5540d53e2e6a25769b7fae282a93474d6b
Cristian Gutierrez <134895358+HealthyW@users.noreply.github.com>
2025-06-24 01:30:56 -0400

    maqueta base carrito lista

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { CatalogueComponent } from './pages/catalogue/catalogue.component';
+import { PaymentComponent } from './pages/payment/payment.component';
 
 export const routes: Routes = [
   {path:'',redirectTo:'home', pathMatch:'full'},
   {path:'home', component:HomeComponent},
   {path:'register',component:RegisterComponent},
-  {path:'catalogue', component:CatalogueComponent}
+  {path:'catalogue', component:CatalogueComponent},
+  {path:'payment',component:PaymentComponent}
 ];
--- src/app/pages/payment/payment.component.ts
+++ src/app/pages/payment/payment.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
 
 @Component({
   selector: 'app-payment',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './payment.component.html',
   styleUrl: './payment.component.css'
 })
